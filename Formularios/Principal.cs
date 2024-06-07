d7a64d7df81a60bb047b814958c0f0fd36f7e884
Jenni2859 <jenifer.garcia.2859@gmail.com>
2024-06-07 14:55:35 -0600

    Cambios para realizar la compra

--- Formularios/Principal.cs
+++ Formularios/Principal.cs
@@ -33,7 +33,7 @@ namespace ClasesINA.Formularios
         {
             string nombreProduct = Producto.Text;
             int unidades = Convert.ToInt32(Unidades.Value);
-            double precioUnidad = Convert.ToDouble(precio.Text);
+            double precioUnidades = Convert.ToDouble(precio.Text);
             double total = Convert.ToDouble(TotalPrecio.Text);
 
             if (total <=0 || precioUnidad <=0)
@@ -47,8 +47,25 @@ namespace ClasesINA.Formularios
                 {
                     MessageBox.Show("El nombre no debe de ser cero");
                 }
-                else //
+                else // se realiza la inserción y se redimenciona los vectores
                 {
+                    Array.Resize(ref Productos, Productos.Length +1);
+                    Array.Resize(ref unidadesCompra, unidadesCompra.Length +1);
+                    Array.Resize(ref precioUnidad, precioUnidad.Length +1);
+                    Array.Resize(ref totalpago, totalpago.Length + 1);
+
+                    Productos[Productos.Length - 1] = nombreProduct;
+                    unidadesCompra[unidadesCompra.Length - 1] = unidades;
+                    precioUnidad[precioUnidad.Length - 1] = precioUnidades;
+                    totalpago[totalpago.Length - 1] = total;
+
+                    Producto.Text = "";
+                    Unidades.Value = 0;
+                    precio.Text = "";
+                    TotalPrecio.Text = "0.0";
+
+                    MessageBox.Show("Compra efectuada con éxito");
+                    Producto.Focus();
 
                 }
             }
